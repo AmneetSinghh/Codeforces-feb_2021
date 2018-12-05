6f67f3e8ed01fb54c1c5114db233616c4225679f
Harry_122 <harryamneet600@gmail.com>
2018-12-05 21:20:00 +0530

    Add solution for problem `C - Minimal string` Link: https://codeforces.com/contest/797/problem/C Tags: data structures, greedy, strings, *1700 Ref: https://codeforces.com/contest/797/submission/46655025

--- codeforces/797/C.cpp
+++ codeforces/797/C.cpp
@@ -1,24 +1,23 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
+const int Maxn=1e5+7;
 string s;
-char temp[100100];
-stack<char> stak;
-int main () {
-    cin>>s;
-    temp[s.size()]='z'+1;
-    for (int i=s.size()-1; i>=0; i--) 
-        temp[i]=min(temp[i+1], s[i]);
-
-        
-
-        
-    int j=0;
-    for (int i=0; i<(int)s.size(); i++) {
-        stak.push(s[i]);
-        while (!stak.empty() &&stak.top()<=temp[i+1]){ 
-            cout<<stak.top();stak.pop();}
-    }
-    return 0;
+char c[Maxn];
+stack <char> st;
+int main()
+{
+	cin>>s;
+	c[(int)s.size()]='z';
+	for(int i=s.size()-1;i>=0;i--)
+		c[i]=min(c[i+1],s[i]);
+		
+	for(int i=0;i<(int)s.size();i++){
+		while(!st.empty() && st.top()<=c[i])
+			cout<<st.top(),st.pop();
+		st.push(s[i]);
+	}
+	while(!st.empty()){
+		cout<<st.top();
+		st.pop();
+	}
 }
