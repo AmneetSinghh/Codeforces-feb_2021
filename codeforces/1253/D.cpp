b158497c169073c398bd485204cb3fbbbbd7f24d
Harry_122 <harryamneet600@gmail.com>
2019-11-17 10:23:00 +0530

    Add solution for problem `D - Harmonious Graph` Link: https://codeforces.com/contest/1253/problem/D Tags: constructive algorithms, dfs and similar, dsu, graphs, greedy, sortings, *1700 Ref: https://codeforces.com/contest/1253/submission/65230756

--- codeforces/1253/D.cpp
+++ codeforces/1253/D.cpp
@@ -1,38 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define N 200007
-int par[N], R[N];
- 
-int find(int x) { return par[x] = (par[x]==x?x:find(par[x])); }
-void un(int u, int v) 
-{
-    u = find(u); v = find(v);
-    par[v] = u;
-    R[u] = max(R[u], R[v]);
+typedef long long ll;
+vector<int>g[200009];
+ll n,m,x,y,ans,mx,vis[200009];
+void dfs(ll v){
+		vis[v]=1;
+    mx=max(v,mx);// max vertd i hagr to find bro
+    for(int i=0;i<g[v].size();i++) 
+	if(!vis[g[v][i]]) 
+	 dfs(g[v][i]);
 }
- 
 int main()
 {
-    int n,m;
     cin>>n>>m;
-    for(int i=0;i<N;i++) par[i] = i, R[i] = i;
     for(int i=0;i<m;i++)
-    {
-        int u,v;
-        cin>>u>>v;
-        un(u,v);
+	{
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
- 
-
- 
-    int cnt = 0;
-    for(int i=1;i<n;i++) 
-    {
-        if (find(i) != find(i+1) && R[find(i)] > i+1) 
-        {
-            un(i,i+1);
-            cnt++;
+    for(int i=1;i<=n;i++)
+	{
+        if(!vis[i])
+		{
+            if(mx>i)ans++;
+            dfs(i);
         }
     }
-    cout << cnt << endl;
+    cout<<ans;
 }
