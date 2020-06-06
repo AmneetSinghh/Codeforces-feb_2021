f7f90f0b7adf490d573f920e4947e153d2549c0e
Harry_122 <harryamneet600@gmail.com>
2020-06-06 17:41:00 +0530

    Add solution for problem `B - Non-Coprime Partition` Link: https://codeforces.com/contest/1038/problem/B Tags: constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1038/submission/82702506

---
+++ codeforces/1038/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ #define pb push_back
+#define int long long
+#define endl '\n'
+#define maxn 500005
+
+int32_t main(){
+// int t;cin>>t;while(t--){
+int n,k;
+cin>>n;
+if(n<=2)
+cout<<"No\n";
+else
+{
+	cout<<"Yes\n";
+    cout<<"1 "<<n<<"\n";
+    cout<<n-1<<" ";
+    	for(int i=1;i<=n-1;i++)
+	     cout<<i<<" ";
+	     
+	     cout<<"\n";
+}
+	
+	
+}
+
+
+	
+	
+	
+	
+
