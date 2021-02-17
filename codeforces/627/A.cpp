d45a5c44e13947099718530d8b23444b0295490e
Harry_122 <harryamneet600@gmail.com>
2021-02-17 20:57:00 +0530

    Add solution for problem `A - XOR Equation` Link: https://codeforces.com/contest/627/problem/A Tags: dp, math, *1700 Ref: https://codeforces.com/contest/627/submission/107706034

---
+++ codeforces/627/A.cpp
@@ -0,0 +1,56 @@
+/* It's Practice time */
+#include<bits/stdc++.h>
+#define int long long
+#define io ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define ipair pair<int,int>
+#define pb push_back
+#define f(i, n) for(int i=0;i<n;i++)
+#define F(i, n) for(int i=1;i<=n;i++)
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define pre(i,a,b) for(int i=a;i>=b;i--)
+#define all(v) v.begin(),v.end()
+#define tr cout<<"\n"
+#define br break
+#define mod 1000000000000000000
+#define inf 1000000000009
+using namespace std;
+
+/*
+Imp Observations: s=a+b=(a^b)+(a&b)*2;
+(a&b)=((a+b)-(a^b))/2
+*/
+
+void Not_Stable()
+{
+  int a,b;
+  cin>>a>>b;
+  int diff=a-b;
+  if(diff<0 || diff%2==1){
+    cout<<"0";tr;
+    return;
+  }
+  diff/=2;
+  int ans=1;// all the possilble cases, if a =101010 b=10101 check we can take one in both of these positions
+  for(int i=0;i<=40;i++){
+    if(b&(1ll<<i))ans*=2;
+    if(diff&(1ll<<i)){
+      if(b&(1ll<<i)){
+        cout<<"0";tr;;return;
+      }
+    }
+
+  }
+  if(a==b)ans-=2;// for the case whhen a =b that means, one can be zerol
+  cout<<ans;
+  tr;
+}
+
+
+
+int32_t main() {
+    io;
+    int g=1;
+    while (g--)Not_Stable();
+    return 0;
+    
+}
